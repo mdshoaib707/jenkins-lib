10ffd9d8d4148d6a93f2edae42b8b79e1f111190
mdshoaib707 <mdshoaib707@gmail.com>
2020-07-05 02:54:18 +0530

    Updated helm.upgrade

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -23,11 +23,59 @@ def upgrade(Map config) {
             throw new Exception("Value overrides file ${value} does not exist!")
         }
         cmd += " -f ${value}"
+      }
     }
+    
+    if (config.set != null) {
+      if (!(config.set instanceof String[]) && !(config.set instanceof List<String>)) {
+        throw new Exception('The set parameter must be an array/list of strings.')
+      }
+      config.set.each() { kv ->
+        cmd += " --set ${kv}"
+      }
+    }
+    
+    if (config.reuseValues == true) {
+      cmd += " --reuse-values"
+    }
+    if (config.context != null) {
+      cmd += " --kube-context ${config.context}"
+    }
+    if (config.namespace != null) {
+      cmd += " --namespace ${config.namespace}"
+    }
+    if (config.wait == true) {
+      cmd += " --wait"
+    }
+    if (config.verify == true) {
+      cmd += " --verify"
+    }
+    if (config.debug == true) {
+      cmd += " --debug"
+    }
+    if (config.recreatePods == true) {
+      cmd += " --recreate-pods"
+    }
+    if (config.force == true) {
+      cmd += " --force"
+    }
+    if (config.timeout) {
+      cmd += " --timeout ${config.timeout}"
+    }
+    
+    cmd += " --install ${config.name} ${config.chart}"
+    if (config.debug == true) {
+      echo "[INFO] helm.upgrade - runing helm cli: ${cmd}"
+    }
+    
+    sh cmd
+    
   }
   catch (Exception error) {
+    echo '[ERROR] helm.upgrade - Failure using helm upgrade.'
+    throw error
   }
-  finally {
-  }
+  
+  echo '[INFO] helm.upgrade - Helm upgrade executed successfully.'
   
 }
